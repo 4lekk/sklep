42ae4ae12477a6020252151690421bd63c8499c1
4lekk <eniekdalk@gmail.com>
2023-06-30 11:51:27 +0200

    dolaczenie thymeleafa; dzialajaca wersja sklepu; dodanie mozliwosci sortowania produktow

--- src/main/java/com/sklepwjavie/DbAppController.java
+++ src/main/java/com/sklepwjavie/DbAppController.java
@@ -1,28 +1,121 @@
 package com.sklepwjavie;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
-@RestController
-@RequestMapping("/items")
+@Controller
+//@RequestMapping("/items")
 public class DbAppController {
+
+    List<String> sortModes =
+            Arrays.asList("nazwa", "cena", "waga", "data");
+    Comparator<Product> nameSort = new Comparator<Product>() {
+        @Override
+        public int compare(Product o1, Product o2) {
+            if (o1.getName().compareToIgnoreCase(o2.getName()) < 0) {
+                return -1;
+            }
+            else if (o1.getName().compareToIgnoreCase(o2.getName()) > 0) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+    };
+    Comparator<Product> priceSort = new Comparator<Product>() {
+        @Override
+        public int compare(Product o1, Product o2) {
+            var bd = new BigDecimal(o1.getPrice() - o2.getPrice())
+                    .setScale(0, RoundingMode.UP);
+            return bd.intValueExact();
+        }
+    };
+    Comparator<Product> weightSort = new Comparator<Product>() {
+        @Override
+        public int compare(Product o1, Product o2) {
+            var bd = new BigDecimal(o1.getWeight() - o2.getWeight())
+                    .setScale(0, RoundingMode.UP);
+            return bd.intValueExact();
+        }
+    };
+    Comparator<Product> dateSort = new Comparator<Product>() {
+        @Override
+        public int compare(Product o1, Product o2) {
+            return o1.getFullDate().compareTo(o2.getFullDate());
+        }
+    };
     private final ProductService pService;
 
     public DbAppController(ProductService p) {
         pService = p;
     }
 
-    @GetMapping("")
-    public List<Product> getAllProducts() {
-        return pService.getAllProducts();
+    @GetMapping("index")
+    public String getAllProducts(Model model) {
+        model.addAttribute("products", pService.getAllProducts());
+        model.addAttribute("sort", nameSort);
+        model.addAttribute("sortModes", sortModes);
+        model.addAttribute("dropDownOptionLabel", "nazwa");
+        return "index";
     }
-    @GetMapping("{id}")
+
+    @GetMapping("index/nazwa")
+    public String getAllProductsSortedByName(Model model) {
+        model.addAttribute("products", pService.getAllProducts());
+        model.addAttribute("sort", nameSort);
+        model.addAttribute("sortModes", sortModes);
+        model.addAttribute("dropDownOptionLabel", "nazwa");
+        return "index";
+    }
+
+    @GetMapping("index/cena")
+    public String getAllProductsSortedByPrice(Model model) {
+        model.addAttribute("products", pService.getAllProducts());
+        model.addAttribute("sort", priceSort);
+        model.addAttribute("sortModes", sortModes);
+        model.addAttribute("dropDownOptionLabel", "cena");
+        return "index";
+    }
+
+    @GetMapping("index/waga")
+    public String getAllProductsSortedByWeight(Model model) {
+        model.addAttribute("products", pService.getAllProducts());
+        model.addAttribute("sort", weightSort);
+        model.addAttribute("sortModes", sortModes);
+        model.addAttribute("dropDownOptionLabel", "waga");
+        return "index";
+    }
+
+    @GetMapping("index/data")
+    public String getAllProductsSortedByDate(Model model) {
+        model.addAttribute("products", pService.getAllProducts());
+        model.addAttribute("sort", dateSort);
+        model.addAttribute("sortModes", sortModes);
+        model.addAttribute("dropDownOptionLabel", "data");
+        return "index";
+    }
+
+
+
+
+
+//    @GetMapping("css/style.css")
+//    public String getStyle(Model model) {
+//        return "../css/style.css";
+//    }
+    @GetMapping("items/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable Long id) {
         Optional<Product> p = pService.getProductById(id);
         if (p.isEmpty()) {
@@ -31,36 +124,66 @@ public class DbAppController {
         return ResponseEntity.status(HttpStatus.OK).body(p.get());
     }
 
-    @PostMapping("")
-    public ResponseEntity<String> saveProduct(@RequestBody Product product) {
+    @GetMapping("addProductForm")
+    public String addProductForm(Product product) {
+        return "addProductForm";
+    }
+
+    @PostMapping("addProduct")
+    public String addProduct(@Valid Product product,
+                             BindingResult result,
+                             Model model) {
         product.setFullDate(LocalDateTime.now());
         pService.save(product);
-        return new ResponseEntity<String>("Successfully added product", HttpStatus.CREATED);
+        model.addAttribute("products", pService.getAllProducts());
+        model.addAttribute("sort", nameSort);
+        model.addAttribute("sortModes", sortModes);
+        model.addAttribute("dropDownOptionLabel", "waga");
+        return "index";
     }
 
-    @PutMapping("{id}")
-    public ResponseEntity<String> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+    @GetMapping("updateProductForm/{id}")
+    public String updateProductForm(@PathVariable    Long id, Product product, Model model) {
+        model.addAttribute("product", pService.getProductById(id).get());
+//        model.addAttribute("id", id.toString());
+        return "updateProductForm";
+    }
+
+    @GetMapping("updateProduct/{id}")
+    public String updateProduct(@PathVariable Long id,
+                                @Valid Product product,
+                                Model model) {
         Optional<Product> p = pService.update(id, product);
         if (p.isEmpty()) {
-            return new ResponseEntity<>("Product with id " + id + " not found",
-                    HttpStatus.NOT_FOUND);
+//            return new ResponseEntity<>("Product with id " + id + " not found",
+//                    HttpStatus.NOT_FOUND);
+
         }
         else {
-            return new ResponseEntity<>("Successfully updated product with id " + id,
-                    HttpStatus.OK);
+//            return new ResponseEntity<>("Successfully updated product with id " + id,
+//                    HttpStatus.OK);
         }
+        model.addAttribute("sortModes", sortModes);
+        model.addAttribute("dropDownOptionLabel", "waga");
+        model.addAttribute("products", pService.getAllProducts());
+        model.addAttribute("sort", nameSort);
+        return "index";
     }
 
-    @DeleteMapping("{id}")
-    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+    @GetMapping("removeProduct/{id}")
+    public String deleteProduct(@PathVariable Long id, Product product, Model model) {
         Optional<Product> optionalProduct = pService.remove(id);
         if (optionalProduct.isEmpty()) {
-            return new ResponseEntity<String>("Product with id " + id + " not found",
-                    HttpStatus.NOT_FOUND);
+//            return new ResponseEntity<String>("Product with id " + id + " not found",
+//                    HttpStatus.NOT_FOUND);
         }
         else {
-            return new ResponseEntity<String>("Product with id " + id + " successfully deleted",
-                    HttpStatus.OK);
+
         }
+        model.addAttribute("products", pService.getAllProducts());
+        model.addAttribute("sort", nameSort);
+        model.addAttribute("sortModes", sortModes);
+        model.addAttribute("dropDownOptionLabel", "waga");
+        return "index";
     }
 }
--- src/main/java/com/sklepwjavie/SklepApplication.java
+++ src/main/java/com/sklepwjavie/SklepApplication.java
@@ -1,9 +1,12 @@
 package com.sklepwjavie;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class SklepApplication {
 
 	public static void main(String[] args) {
